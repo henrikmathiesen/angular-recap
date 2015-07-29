c4fdc3584b4210a20993a811916ed4244c0edd50
Henrik <henrik_mathiesen@hotmail.com>
2015-07-29 20:47:41 +0200

    js patterns

--- js/jspatterns.js
+++ js/jspatterns.js
@@ -1,6 +1,7 @@
 // Basic JavaScript Patterns
 
 
+
 // Sending a function to a function to execute
 
 var work = function(){
@@ -17,7 +18,7 @@ doWork(work);
 
 
 
-// Revealing Module Pattern, crude
+// Revealing Module Pattern
 
 var createWorker = function(){
 	
@@ -43,3 +44,35 @@ var worker = createWorker();
 worker.job01();
 worker.job02();
 worker.job01();
+
+
+//  ^ All these global variables is not ok
+
+
+
+// Revealing Module Pattern with self executing function
+
+var ajaxFactory = (function(){
+	
+	var _ajaxCallCount = 0;
+	
+	var ajaxCall = function(){
+		_ajaxCallCount += 1;
+		console.log("Doing Ajax Call " + _ajaxCallCount);
+	};
+	
+	
+	return {
+		ajaxCall: ajaxCall
+	}
+	
+})();
+
+(function(){
+	ajaxFactory.ajaxCall();
+	ajaxFactory.ajaxCall();
+	ajaxFactory.ajaxCall();
+})();
+
+// This example has one global variable, 'ajaxFactory', with a descriptive name, so we can reach it from other files
+// In those files we encapsulate the script in an IIFE (immediately invoked function expression)
