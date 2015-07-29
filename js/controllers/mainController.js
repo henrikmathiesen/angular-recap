28c762fd6c3634b888f0ff22030f14f9bc3c3ae8
Henrik <henrik_mathiesen@hotmail.com>
2015-07-29 21:21:23 +0200

    Doing controllers

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,5 +1,19 @@
 /// <reference path="../../typings/angularjs/angular.d.ts"/>
 
-var mainController = function($scope){
+angular.module('angularRecap').controller('mainController', ['$scope', function($scope){
+		$scope.message = "This is a min safed controller";
+}]);
+
+
+// Another way, perhaps more clean
+
+(function(){
 	
-};
+	var mainControllerToo = function($scope){
+		$scope.message = "This is also a min safed controller";
+	};
+	
+	angular.module('angularRecap').controller('mainControllerToo', mainControllerToo);
+	mainControllerToo.$inject = ['$scope'];
+	
+})();
