55e9447242b57e44f81b7e19cc3654272d19e324
Henrik <henrik_mathiesen@hotmail.com>
2015-07-31 04:21:13 +0200

    Added some comments

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,3 +1,7 @@
 /// <reference path="../../typings/angularjs/angular.d.ts"/>
 
+// We dont store this in an 'app' variable, to avoid global variables
+// We can get to this module like this instead
+// angular.module('angularRecap').controller(...)
+// http://stackoverflow.com/questions/17035890/angular-convention-for-declaring-controllers
 angular.module('angularRecap', []);
