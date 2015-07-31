86440253bacb70aad9382e5aacee43be2119eb74
Henrik <henrik_mathiesen@hotmail.com>
2015-07-31 07:33:54 +0200

    searching users

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -16,16 +16,27 @@ angular.module('angularRecap').controller('mainController', ['$scope', function(
 			lastName: 'Doe',
 			image: 'http://lorempixel.com/output/cats-q-c-640-480-1.jpg'
 		};
+		$scope.query = "";
 		$scope.user = {};
 		$scope.error = '';
 		
-		$http.get('https://api.github.com/users/octocat')
-				.then(function(response){
-					console.log(response.data);
-					$scope.user = response.data;
-				}, function(response){
-					$scope.error = "Error fetching the user"
-				});
+		
+		var makeAjaxCall = function(){
+			$http.get('https://api.github.com/users/' + ($scope.query || 'octocat'))
+					.then(function(response){
+						console.log(response.data);
+						$scope.user = response.data;
+					}, function(response){
+						$scope.error = "Error fetching the user"
+					});
+		};
+		
+		$scope.searchUser = function(){
+			makeAjaxCall();	
+		};
+		
+		
+		makeAjaxCall();
 	};
 	
 	angular.module('angularRecap').controller('mainControllerToo', mainControllerToo);
--- js/modules/app.js
+++ js/modules/app.js
@@ -1,7 +1,6 @@
 /// <reference path="../../typings/angularjs/angular.d.ts"/>
 
 // We dont store this in an 'app' variable, to avoid global variables
-// We can get to this module like this instead
-// angular.module('angularRecap').controller(...)
+// We can get to this module like this instead angular.module('angularRecap').controller(...)
 // http://stackoverflow.com/questions/17035890/angular-convention-for-declaring-controllers
 angular.module('angularRecap', []);
