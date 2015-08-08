ad07152d58b07cf36216012267ebd60dd6315351
Henrik <henrik_mathiesen@hotmail.com>
2015-08-08 10:37:49 +0200

    new restructure with more views

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,23 +1,19 @@
 /// <reference path="../../typings/angularjs/angular.d.ts"/>
 
+/*
 angular.module('angularRecap').controller('mainController', ['$scope', function($scope){
 		$scope.message = "This is a min safed controller";
 }]);
+*/
 
 
 // Another way, perhaps more clean regarding min safing
 
 (function(){
 	
-	var mainControllerToo = function($scope, $interval, $log, $location, $anchorScroll, gitHubService){
+	var mainController = function($scope, $interval, $log, $location, $anchorScroll, gitHubService){
 		var interval = null;
 		
-		$scope.message = "This is also a min safed controller";
-		$scope.person = {
-			firstName: 'John',
-			lastName: 'Doe',
-			image: 'http://lorempixel.com/output/cats-q-c-640-480-1.jpg'
-		};
 		$scope.query = 'octocat';
 		$scope.countdown = 5;
 		$scope.user = null;
@@ -66,7 +62,7 @@ angular.module('angularRecap').controller('mainController', ['$scope', function(
 		//makeUserAjaxCall();
 	};
 	
-	angular.module('angularRecap').controller('mainControllerToo', mainControllerToo);
-	mainControllerToo.$inject = ['$scope', '$interval', '$log', '$location', '$anchorScroll', 'gitHubService'];
+	angular.module('angularRecap').controller('mainController', mainController);
+	mainController.$inject = ['$scope', '$interval', '$log', '$location', '$anchorScroll', 'gitHubService'];
 	
 })();
